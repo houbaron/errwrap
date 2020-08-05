24753899769a4d173e3fc9907fb1eaacca1301bb
baronhou <baronhou@tencent.com>
2020-08-05 16:05:20 +0800

    还是回到了用 struct 封装的老路

--- errwrap.go
+++ errwrap.go
@@ -1,20 +1,43 @@
 package errwrap
 
-type handlerType func(v interface{})
+type (
+	HandlerType func(v interface{})
 
-func Handle(handler handlerType, errs ...error) {
+	Closer interface {
+		Close() error
+	}
+
+	Wrapper struct {
+		Handler HandlerType
+	}
+)
+
+func (wrapper *Wrapper) handle(errs ...error) {
 	for _, err := range errs {
 		if err != nil {
-			handler(err)
+			wrapper.Handler(err)
 		}
 	}
 }
 
-func ReturnResult(handler handlerType, result interface{}, err error) interface{} {
-	Handle(handler, err)
+func (wrapper *Wrapper) ReturnResult(result interface{}, err error) interface{} {
+	wrapper.handle(err)
 	return result
 }
 
-func IgnoreResult(handler handlerType, _ interface{}, err error) {
-	Handle(handler, err)
+func (wrapper *Wrapper) IgnoreResult(_ interface{}, err error) {
+	wrapper.handle(err)
 }
+
+func (wrapper *Wrapper) Close(closer Closer) {
+	if closer != nil {
+		wrapper.handle(closer.Close())
+	}
+}
+
+var (
+	PanicWrapper   = &Wrapper{Handler: panic}
+	PrintLnWrapper = &Wrapper{Handler: func(v interface{}) {
+		println(v)
+	}}
+)
