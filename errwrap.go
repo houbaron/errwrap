e7fa47ee4e038914fc5fd3b075a784f983258cf1
baronhou <baronhou@tencent.com>
2020-08-05 14:47:51 +0800

    简单化

--- errwrap.go
+++ errwrap.go
@@ -1,9 +1,30 @@
 package errwrap
 
-func BaseHandle(handle func(...interface{}), errs ...error) {
+type handlerType func(v interface{})
+
+var handler handlerType = panic
+
+func SetHandler(f handlerType) {
+	handler = f
+}
+
+func baseHandle(handle handlerType, errs ...error) {
 	for _, err := range errs {
 		if err != nil {
 			handle(err)
 		}
 	}
+}
+
+func Handle(errs ...error) {
+	baseHandle(handler, errs...)
+}
+
+func ReturnResult(result interface{}, err error) interface{} {
+	Handle(err)
+	return result
+}
+
+func IgnoreResult(_ interface{}, err error) {
+	Handle(err)
 }
